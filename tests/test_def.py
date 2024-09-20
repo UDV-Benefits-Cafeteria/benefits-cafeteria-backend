a5cc477fa8068183dac4c1b0f58753c5566e1948
Илья <128465818+Karasik876@users.noreply.github.com>
2024-09-20 19:44:08 +0500

    fix: pre-commit

--- tests/test_def.py
+++ tests/test_def.py
@@ -2,8 +2,9 @@ from httpx import AsyncClient
 
 
 def testing():
-    assert 1==1
-    assert 2==2
+    assert 1 == 1
+    assert 2 == 2
+
 
 async def test2(ac: AsyncClient):
     assert ac is not None
