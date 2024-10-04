721e8aa189a01b2c6624c59a8b279a098bdb7949
Илья <128465818+Karasik876@users.noreply.github.com>
2024-10-04 23:28:41 +0500

    fix: tests

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -4,5 +4,9 @@ from src.api.v1 import default
 from src.api.v1.fake import benefit
 
 router = APIRouter()
-router.include_router(default.router, tags=["default"], prefix="/test",)
+router.include_router(
+    default.router,
+    tags=["default"],
+    prefix="/test",
+)
 router.include_router(benefit.router, tags=["fake"])
--- tests/test_ping.py
+++ tests/test_ping.py
@@ -3,10 +3,10 @@ from httpx import AsyncClient
 
 
 async def test_ping(async_client: AsyncClient):
-    response = await async_client.get("/api/v1/ping")
+    response = await async_client.get("/api/v1/test/ping")
     assert response.json() == {"success": True}
 
 
 async def test_index(async_client: AsyncClient):
-    response = await async_client.get("/api/v1/")
+    response = await async_client.get("/api/v1/test/")
     assert response.json() == {"index": True}
