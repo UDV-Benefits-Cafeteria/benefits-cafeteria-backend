286ae851db716db1f8d42440362235b3489adbb0
StalinidzeCorp <guidemaster228@gmail.com>
2024-10-17 21:32:13 +0500

    fix: suppres email while debug

--- src/utils/email.py
+++ src/utils/email.py
@@ -18,7 +18,7 @@ conf = ConnectionConfig(
     MAIL_SSL_TLS=settings.MAIL_SSL_TLS,
     USE_CREDENTIALS=settings.MAIL_USE_CREDENTIALS,
     VALIDATE_CERTS=settings.MAIL_VALIDATE_CERTS,
-    SUPPRESS_SEND=False,
+    SUPPRESS_SEND=settings.DEBUG,
 )
 
 fm = FastMail(conf)
