7141ff2ae52fe461af6f47b3650fa2f853ec7afd
Yurchenko Stanislav <yurchenko.stas15@gmail.com>
2024-11-18 19:50:23 +0500

    docs(UDV-188): clear alembic env file

--- alembic/env.py
+++ alembic/env.py
@@ -9,29 +9,18 @@ import src.models as models
 from alembic import context
 from src.config import get_settings
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 settings = get_settings()
 
 
 section = config.config_ini_section
 config.set_section_option(section, "DATABASE_URL", settings.DATABASE_URL)
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = models.Base.metadata
 
 
 def run_migrations_offline() -> None:
