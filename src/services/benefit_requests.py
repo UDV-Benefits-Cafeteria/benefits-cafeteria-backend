4fc29c238bd64dfb974c840c6f95f4380fba058f
Yurchenko Stanislav <yurchenko.stas15@gmail.com>
2024-12-05 02:08:28 +0500

    fix: new performer of request

--- src/services/benefit_requests.py
+++ src/services/benefit_requests.py
@@ -340,10 +340,7 @@ class BenefitRequestsService(
                     )
 
                 # If old status is 'pending' then the request was just created and needs performer_id to be set
-                if new_status == schemas.BenefitStatus.PENDING:
-                    update_schema.performer_id = None
-
-                if new_status == schemas.BenefitStatus.PROCESSING:
+                if old_status.value == schemas.BenefitStatus.PENDING:
                     update_schema.performer_id = current_user.id
 
                 # These users have permission to edit the request:
